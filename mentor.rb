d393aeafbe18a27d8c35fd34b86b07bc9462e0f1
KK <kentech.1x1x3@gmail.com>
2021-07-11 12:25:43 +0000

    kadai-ruby-oop-2

---
+++ mentor.rb
@@ -0,0 +1,28 @@
+# mentorクラスの定義
+class Mentor
+    attr_accessor :name
+    
+    def initialize(name)
+        self.name = name
+    end
+    
+    def job
+        puts "#{self.name}です。私は現役のITプロフェッショナルです。"
+    end
+end
+
+#RailsMentorクラスの定義
+
+class RailsMentor < Mentor
+    
+    def job
+        puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+    end
+    
+end
+
+kirameki = Mentor.new("煌木")
+akaide = RailsMentor.new("赤出")
+
+kirameki.job
+akaide.job
